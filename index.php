393590db7c0c9e09db1d759f5c472e771b1458e1
luisaaan <125052371@upq.edu.mx>
2026-09-17 15:31:44 -0600

    HTML y PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- <?php
+    echo "Hello, World!"
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1>HOLA CRAYOLA</h1>
+    <h2>HOLA CRAYOLA</h2>
+    <h3>HOLA CRAYOLA</h3>
+    <h4>HOLA CRAYOLA</h4>
+    <h5>HOLA CRAYOLA</h5>
+    <h6>HOLA CRAYOLA</h6>
+</body>
+</html>
