4c05be5063855ae1c9e6285c8fc02507c1894c1b
luisaaan <125052371@upq.edu.mx>
2026-09-18 17:11:09 -0600

    HTML, CSS y BoxModel

--- index.php
+++ index.php
@@ -12,6 +12,19 @@
         h1{
             color: blue;
         }
+        .seccion{
+            padding: 20px 40px;
+            background-color: aquamarine;
+            border: 10px hidden black;
+        }
+        .seccion2{
+            padding-top: 20px;
+            padding-left: 40px;
+            padding-right: 40px;
+            padding-bottom: 20px;
+            background-color: pink;
+            margin: 20px;
+        }
     </style>
 </head>
 <body>
@@ -21,5 +34,20 @@
     <h4>HOLA CRAYOLA</h4>
     <h5>HOLA CRAYOLA</h5>
     <h6>HOLA CRAYOLA</h6>
+
+    <div class="seccion">
+        <h2>Título del párrafo</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Accusantium quod vel, consequuntur non, ipsa tempora quaerat 
+            ex voluptatum animi iste cumque porro perspiciatis dignissimos 
+            doloremque, consectetur cum deserunt natus vitae.</p>
+    </div>
+    <div class="seccion2">
+        <h2>Título del párrafo</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Accusantium quod vel, consequuntur non, ipsa tempora quaerat 
+            ex voluptatum animi iste cumque porro perspiciatis dignissimos 
+            doloremque, consectetur cum deserunt natus vitae.</p>
+    </div>
 </body>
 </html>
